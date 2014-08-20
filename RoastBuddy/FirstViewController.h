e383693b8ac01a1309226904722dd531e1e98b9d
John Koerner <john@etudesoft.com>
2014-08-20 11:56:57 -0700

    Initial Commit

---
+++ RoastBuddy/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  RoastBuddy
+//
+//  Created by John Koerner on 8/20/14.
+//  Copyright (c) 2014 Etude Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ RoastBuddy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RoastBuddy
+//
+//  Created by John Koerner on 8/20/14.
+//  Copyright (c) 2014 Etude Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
